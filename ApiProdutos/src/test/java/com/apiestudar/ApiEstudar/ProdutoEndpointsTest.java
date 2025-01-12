6461464d2d02b3971f930ccea2e22777925d84a9
Matheus Reynard <matheusreynardkolling2@gmail.com>
2025-01-12 09:25:50 -0300

    feat: adicao d testes unitarios

---
+++ ApiProdutos/src/test/java/com/apiestudar/ApiEstudar/ProdutoEndpointsTest.java
@@ -0,0 +1,99 @@
+package com.apiestudar.ApiEstudar;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import com.apiestudar.model.Produto;
+import com.apiestudar.repository.ProdutoRepository;
+import com.apiestudar.service.ProdutoServiceImpl;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import junit.framework.TestCase;
+
+@SpringBootTest
+public class ProdutoEndpointsTest extends TestCase {
+	
+	@Mock
+	private ProdutoRepository produtoRepository;
+	
+	@InjectMocks
+	private ProdutoServiceImpl produtoService;
+	
+	// Teste que valida o método do service de adicionar um produto no banco de dados
+	@Test
+	public void adicionarProdutoTest() {
+
+		Produto produtoTeste = new Produto();
+		produtoTeste.setId(1);
+		produtoTeste.setNome("Produto teste");
+		produtoTeste.setDescricao("Descricao");
+		produtoTeste.setFrete(10.0);
+		produtoTeste.setPromocao(false);
+		produtoTeste.setValorTotalDesc(50.5);
+		produtoTeste.setValorTotalFrete(55.5);
+		produtoTeste.setValor(33.2);
+		produtoTeste.setQuantia(15);
+		produtoTeste.setSomaTotalValores(150);
+		produtoTeste.setFreteAtivo(false);
+		produtoTeste.setValorDesconto(33.2);
+		
+		Produto produtoRetornadoRepository = new Produto();
+		
+		when(produtoRepository.save(produtoTeste)).thenReturn(produtoRetornadoRepository);
+		
+		Produto produtoRetornadoService = produtoService.adicionarProduto(produtoTeste);
+
+		assertEquals(produtoRetornadoService, produtoRetornadoRepository);
+		
+		if (produtoRetornadoService.equals(produtoRetornadoRepository))
+			System.out.println("TESTE 1: Passou. Produto retornado igual ao enviado.");
+		else
+			System.out.println("TESTE 1: NÃO Passou. Produto retornado diferente do enviado");
+		
+	}
+	
+	// Teste que valida o calculo de desconto retornando o cáculo correto
+	@Test
+	public void calcularValorDescontoTrueTest() {
+			
+		double valorProduto = 120;
+		double valorDesconto = 50; // em porcentagem
+		
+		double valorRetornado = produtoService.calcularValorDesconto(valorProduto, valorDesconto);
+		double valorResultadoEsperado = 60;
+
+		// Retorna TRUE no teste
+		assertEquals(valorResultadoEsperado, valorRetornado);
+		
+		if (valorResultadoEsperado == valorRetornado)
+			System.out.println("TESTE 2: Passou. Resultados iguais.");
+		else
+			System.out.println("TESTE 2: NÃO Passou. Resultados diferentes.");
+		
+	}
+	
+	// Teste que valida o calculo de desconto retornando o cálculo incorreto
+	@Test
+	public void calcularValorDescontoFalseTest() {
+			
+		double valorProduto = 120;
+		double valorDesconto = 50; // em porcentagem
+		
+		// o valorRetornado deve ser 60
+		double valorRetornado = produtoService.calcularValorDesconto(valorProduto, valorDesconto);
+		double valorResultadoErrado = 70;
+		
+		// Retorna FALSE no teste
+		assertNotEquals(valorResultadoErrado, valorRetornado);
+		
+		if (valorResultadoErrado != valorRetornado)
+			System.out.println("TESTE 3: Passou. Resultados diferentes.");
+		else
+			System.out.println("TESTE 3: NÃO Passou. Resultados iguais.");
+	}
+	
+}
