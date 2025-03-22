631adb57c31340c7e85daa54345e6c55a4c83956
Matheus Reynard <matheusreynardkolling2@gmail.com>
2025-03-22 17:28:39 -0300

    fix: estilizacao e formatacao pagina sobre e botao O|O

--- ProdutosAngular/src/app/components/sobre/sobreTab1.component.ts
+++ ProdutosAngular/src/app/components/sobre/sobreTab1.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-sobre',
   imports: [
     RouterLink,
-    RouterLinkActive
+    MatCardModule
   ],
   templateUrl: './sobreTab1.component.html',
   styleUrl: './sobreTab1.component.css'
