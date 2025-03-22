1afeb4413201d007ec32115e43cfe11b8205533e
Matheus Reynard <matheusreynardkolling2@gmail.com>
2025-03-22 19:23:44 -0300

    feat: aba sobre sistema

--- ProdutosAngular/src/app/components/sobre/sobreTab2.component.ts
+++ ProdutosAngular/src/app/components/sobre/sobreTab2.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-sobre1',
-  imports: [
-    RouterLink
-  ],
+    imports: [
+        RouterLink,
+        MatCard,
+        MatCardContent,
+        MatCardHeader,
+        MatCardTitle
+    ],
   templateUrl: './sobreTab2.component.html',
   styleUrl: './sobreTab2.component.css'
 })
