1a9a8bab262873a573497f6afccbc12ebc49762f
Matheus Reynard <matheusreynardkolling2@gmail.com>
2025-05-08 16:16:15 -0300

    commit

---
+++ ProdutosAngular/src/app/components/listar-fornecedor/listar-fornecedor.component.ts
@@ -0,0 +1,149 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Produto } from '../../model/produto';
+import { ProdutoService } from '../../service/produto/produto.service';
+import {CurrencyPipe, NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
+import {NgbModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ProdutoFunctionsService } from '../../service/produto/produto-functions.service';
+import {PorcentagemMaskDirective} from '../../directives/porcentagem-mask.directive';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import {AuthService} from '../../service/auth/auth.service';
+import {HttpResponse} from '@angular/common/http';
+import {DeviceService} from '../../service/device/device.service';
+
+@Component({
+  selector: 'app-listar-fornecedor',  // Corrigido para 'app-produto-list', sem a barra inicial
+  templateUrl: './listar-fornecedor.component.html',
+  styleUrls: ['./listar-fornecedor.component.css'],
+  imports: [
+    NgForOf,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    NgxPaginationModule,
+    MatPaginatorModule
+  ]
+})
+
+export class ListarFornecedorComponent implements OnInit {
+
+  // Variáveis
+  tipoPesquisaSelecionado: string = 'id';
+
+  listaDeProdutos!: Produto[];
+  produtoAtualizar!: Produto;
+  produtoExcluido!: Produto;
+
+  // Variáveis de paginação
+  totalRecords: number = 0;
+  currentPage: number = 0;
+  pageSize: number = 10;
+
+  private modalService: NgbModal = new NgbModal();
+  @ViewChild('searchBar') searchBar!: ElementRef
+  @ViewChild('modalAvisoToken') modalAvisoToken!: ElementRef
+  isMobileOrTablet: boolean = false;
+
+  constructor(private produtoService: ProdutoService,
+              private authService: AuthService, private deviceService: DeviceService) { }
+
+  ngOnInit() {
+    this.produtoService.listarProdutos(this.currentPage, this.pageSize).subscribe(data => {
+      const usuarioLogadoId = this.authService.getUsuarioLogado().id;
+      this.listaDeProdutos = data.content.filter(produto => produto.idUsuario === usuarioLogadoId); // Filtra os produtos do usuário logado
+      this.totalRecords = this.listaDeProdutos.length; // Atualiza o total de registros exibidos
+    });
+
+    this.deviceService.isMobileOrTablet.subscribe(isMobile => {
+      this.isMobileOrTablet = isMobile;
+    });
+  }
+
+  /* Função para deletar um produto através do id. Chama o endpoint, e a msg de sucesso
+  deletarProduto(modalDeletar: any, id: number, produto: Produto) {
+    this.produtoService.deletarProduto(id).subscribe({
+      next: (response) => {
+        if (response.status === 200) {
+          this.produtoExcluido = produto;
+          this.abrirTelaExclusao(modalDeletar);
+        }
+      }
+    });
+    this.atualizarLista();
+  }*/
+
+  /* Função para atualizar um produto através do id
+  atualizarProduto(janelaEditar: any, id: number, produto: Produto) {
+    this.produtoService.atualizarProduto(id, produto, this.imagemFile).subscribe({
+      next: (produtoRetornado: Produto) => {
+        this.produtoAtualizar = produtoRetornado
+        this.calcularValores(this.produtoAtualizar);
+        this.abrirTelaEdicao(janelaEditar)
+      }
+    })
+  }*/
+
+  // Função que atualiza a lista de produtos
+  atualizarLista(): void {
+    this.produtoService.listarProdutos(this.currentPage, this.pageSize).subscribe(data => {
+      const usuarioLogadoId = this.authService.getUsuarioLogado().id;
+      this.listaDeProdutos = data.content.filter(produto => produto.idUsuario === usuarioLogadoId); // Filtra os produtos do usuário logado
+      this.totalRecords = this.listaDeProdutos.length; // Atualiza o total de registros exibidos
+    });
+  }
+
+  trocarPagina(event: PageEvent): void {
+    this.currentPage = event.pageIndex;
+    this.atualizarLista();
+  }
+
+/*
+  // Função que abre o modal - Janela de edição de produto
+  abrirTelaEdicao(janelaEditar: any) {
+    this.modalService.open(janelaEditar, { size: 'lg', backdrop: 'static' });
+  }
+
+  // Função que abre o modal - Janela de exclusão de produto
+  abrirTelaExclusao(modalExcluir: any) {
+    this.modalService.open(modalExcluir);
+    this.listarProdutoMaisCaro(this.authService.getUsuarioLogado().id);
+    this.calcularMedia(this.authService.getUsuarioLogado().id);
+    this.atualizarLista();
+  }
+
+  // Função chamada ao clicar no botão de Submit (Salvar) do formulário de Edição de produtos
+  onSubmitSalvar(modal: any) {
+    this.calcularValores(this.produtoAtualizar);
+    this.produtoService.atualizarProduto(this.produtoAtualizar.id, this.produtoAtualizar, this.imagemFile).subscribe();
+    modal.close();
+  }
+
+// Função chamada ao clicar no botão Pesquisar
+  efetuarPesquisa() {
+    let searchBar_value = this.searchBar.nativeElement.value;
+    const usuarioLogadoId = this.authService.getUsuarioLogado().id;
+
+    this.produtoService.efetuarPesquisa(this.tipoPesquisaSelecionado, searchBar_value, this.authService.getUsuarioLogado().id).subscribe(data => {
+      this.listaDeProdutos = data
+        .filter(produto => produto.idUsuario === usuarioLogadoId) // Filtra os produtos pelo usuário logado
+        .sort((a, b) => a.id - b.id); // Ordena os produtos pelo ID
+    });
+  }
+
+  // Função que abre o modal - Janel de Aviso para Atualizar List de Produtos
+  abrirTelaAviso(modalAviso: any) {
+    this.modalService.open(modalAviso);
+  }
+
+  // Função chamada ao mudar de valor na ComboBox Tipo de Pesquisa
+  trocarTipoPesquisa() {
+    if (this.tipoPesquisaSelecionado =='id' || this.tipoPesquisaSelecionado =='nome') {
+      this.tipoPesquisaSelecionado ='nome'
+    } else {
+      this.tipoPesquisaSelecionado ='id'
+    }
+  } */
+
+}
